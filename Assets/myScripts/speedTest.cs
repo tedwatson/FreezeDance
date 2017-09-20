906bebaf0236eadc76524e870a63f313d70ca819
Ted Watson <tedwatson123@mac.com>
2017-09-20 19:35:05 -0400

    Created project and set up SteamVR plugin

---
+++ Assets/myScripts/speedTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedTest : MonoBehaviour {
+
+    public int checksPerSecond = 10;
+    public int speedThreshold = 5;
+
+    private bool isMoving = false;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(monitorSpeed());
+        StartCoroutine(printStatus());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator printStatus()
+    {
+        while(true)
+        {
+            if (isMoving)
+            {
+                print("Moving");
+            }
+            else
+            {
+                print("Not moving");
+            }
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    IEnumerator monitorSpeed()
+    {
+        Vector3 previousPosition;
+        float speed;
+        while(true)
+        {
+            previousPosition = transform.position;
+            yield return new WaitForSeconds(1f / checksPerSecond);
+            speed = Vector3.Distance(previousPosition, transform.position) / (1f / checksPerSecond);
+            isMoving = speed >= speedThreshold ? true : false;
+        }
+    }
+}
