a186d0448d99b6ffbad4d336f368898de8ac9f30
Ted Watson <tedwatson123@mac.com>
2017-09-21 12:10:31 -0400

    made debug cube to mimic random music

--- Assets/myScripts/movementReportScript.cs
+++ Assets/myScripts/movementReportScript.cs
@@ -9,14 +9,21 @@ namespace Assets.myScripts
     {
         public GameObject minMovementCube;
         public GameObject maxMovementCube;
+        public GameObject songCube;
         public GameObject r_Controller;
         public GameObject l_Controller;
         public GameObject headset;
         public Material green;
         public Material red;
+        public TextMesh gameText;
         public int checksPerSecond = 10;
         public float stillTolerance = 0.04f;
         public float movingTolerance;
+        public float songMin = 2f;
+        public float songMax = 3f;
+        public float freezeMin = 3f;
+        public float freezeMax = 4f;
+        public int beginWaitSeconds = 3;
 
         private Arithmetic arithmetic;
         private const float controllerStillThreshold = 0.029f;
@@ -24,11 +31,71 @@ namespace Assets.myScripts
         private const float controllerMovingThreshold = 0.711f;
         private const float headsetMovingThreshold = 0.201f;
         private bool playerIsMoving;
+        private bool songIsPlaying = false;
 
         void Start()
         {
             arithmetic = new Arithmetic();
+
+            // Have controller triggers activate an method called "Triggered"
+            r_Controller.GetComponent<SteamVR_TrackedController>().TriggerClicked += Triggered;
+            l_Controller.GetComponent<SteamVR_TrackedController>().TriggerClicked += Triggered;
+
+            //temp
+            StartCoroutine(PlayGame());
+        }
+
+        void Triggered(object sender, ClickedEventArgs e)
+        {
+            // Player must pull trigger to begin game
+            StartCoroutine(PlayGame());
+        }
+
+        IEnumerator PlayGame()
+        {
+            // Count off before game begins
+            int secondsUntilGame = beginWaitSeconds;
+            while(secondsUntilGame > 0)
+            {
+                gameText.text = "Music Starting in " + secondsUntilGame + " Seconds";
+                yield return new WaitForSeconds(1);
+                secondsUntilGame--;
+            }
+
+            // Play Music
+            StartCoroutine(PlayMusic());
+
+            // Start checking for motion
             StartCoroutine(checkForMotion());
+        }
+
+        IEnumerator PlayMusic()
+        {
+            // Play music
+            songCube.GetComponent<Renderer>().material = green;
+            songIsPlaying = true;
+
+            while (true)
+            {
+                // Wait a random amount of time
+                float min = songIsPlaying ? songMin : freezeMin;
+                float max = songIsPlaying ? songMax : freezeMax;
+                float waitTime = Random.Range(min, max);
+                print("wait time = " + waitTime);
+                yield return new WaitForSeconds(waitTime);
+
+                // Switch song state
+                if (songIsPlaying)
+                {
+                    songIsPlaying = false;
+                    songCube.GetComponent<Renderer>().material = red;
+                }
+                else
+                {
+                    songIsPlaying = true;
+                    songCube.GetComponent<Renderer>().material = green;
+                }
+            }
         }
 
         IEnumerator checkForMotion()
